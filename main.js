39047c4590a4dd550d6ec4bba88c853bfd0b59a0
Lucas Araújo <lucasaraujodesa@hotmail.com>
2022-05-08 17:01:07 -0300

    Aplicando o responsivo

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+}
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+    
+    // Verificar se a seção passou da targetLine e se a base está abaixo
+    const sectionTop = section.offsetTop
+    const sectionHeigt = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+    const sectionEndsAt = sectionTop + sectionHeigt
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    // Limites da seção
+    const sectionBoundaries = 
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
